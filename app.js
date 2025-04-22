cc21a92495c2df527bfb0ed174400a0e662e51c6
Aeiman-Fayyaz <aeimanfayyaz2002@gmail.com>
2025-04-23 02:03:39 +0500

    code update

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
-let time = new Date()
-time.getHours()
-console.log(time);
+let btn = document.getElementById("btn")
+console.log(btn);
+
+btn.addEventListener("click" , function(){
+    let display = document.getElementsByClassName("display")
+    display.addEventListener("click" , function(){
+        display.value = '' 
+    })
+    
+})
 
