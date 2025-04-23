68ed561ab7a2629c788c55b2e9b717d0a2403227
Aeiman-Fayyaz <aeimanfayyaz2002@gmail.com>
2025-04-24 02:15:47 +0500

    Code update

--- app.js
+++ app.js
@@ -1,11 +1,39 @@
-let btn = document.getElementById("btn")
-console.log(btn);
-
-btn.addEventListener("click" , function(){
-    let display = document.getElementsByClassName("display")
-    display.addEventListener("click" , function(){
-        display.value = '' 
-    })
+// Current Time
+function time(params) {
+    let time = new Date()
+    let hours = new Date().getHours()
+    let minutes = new Date().getMinutes()
+    let seconds = new Date().getSeconds()
     
-})
+    document.getElementById("time").innerHTML = `${hours}:${minutes}:${seconds}` 
+}
+setInterval(time, 500)
 
+// Calculator 
+
+// Input Blank string
+let string = ""
+
+// Buttons Access
+let buttons = document.querySelectorAll(".btn-2")
+
+// Input Field Access
+let input = document.getElementsByTagName("input")
+
+// Logic
+Array.from(buttons).forEach((buttons) => {
+    buttons.addEventListener('click' , function(e){
+        if(e.target.innerHTML == "="){
+            string = eval(string)
+            document.querySelector("input").value = string
+        }
+        else if(e.target.innerHTML == "AC"){
+            string = ""
+            document.querySelector("input").value = string
+        }
+        else{
+            string = string  + e.target.innerHTML;
+            document.querySelector('input').value = string
+        }
+    })
+})
